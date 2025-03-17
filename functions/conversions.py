25a62e892b4f479c44872a0d8ba2f0aac6cb3d9b
jleto6 <jletobar@gmail.com>
2025-03-16 19:20:19 -0500

    Add Vosk model configuration files and implement note reading functionality

---
+++ functions/conversions.py
@@ -0,0 +1,54 @@
+import os
+from pdf2image import convert_from_path
+from PIL import Image
+import magic
+
+# Uploaded Files
+folder = "notes"
+files = os.listdir(folder) # List of files in notes folder 
+if ".DS_Store" in files:
+        files.remove(".DS_Store")
+
+# Folder of converted pngs
+pngs_folder = "converted_pngs" 
+
+# Get The File Type
+def get_file_type(file):
+    file_path = os.path.join(folder, file)  # Get full file path
+    file_type = magic.from_file(file_path, mime=True) # Detects file type
+    file_type = file_type.split("/")[-1]
+    file_type = file_type.upper()
+    print(f"{file_path} | {file_type}")
+    
+    return file_path, file_type
+
+# Handle Images
+def image(file):
+    file_path, _ = get_file_type(file)
+
+    img = Image.open(file_path) # Open the current image 
+    img = img.convert("RGBA") # Convert to RGBA
+
+    # Create the output filename with a .png extention
+    base_name = os.path.splitext(file)[0] + ".png"
+    output_file = os.path.join(pngs_folder, base_name)
+    img.save(output_file, "PNG")
+    #print(f"Output file:{output_file}"
+
+# Handle PDFs
+def pdf(file):
+    file_path, _ = get_file_type(file)
+
+    images = convert_from_path(file_path) # Convert PDF pages to a list of images
+    # Save each page as a PNG in the output folder
+    for i, image in enumerate(images):
+        image.save(os.path.join(pngs_folder, f"page_{i+1}.png"), "PNG")
+
+# Handle Videos
+def video(file):
+    file_path, _ = get_file_type(file)
+
+for file in files:
+    video(file)
+
+    
--- notes.py
+++ notes.py
@@ -11,11 +11,16 @@ import pandas as pd
 from reportlab.lib.pagesizes import letter
 from reportlab.lib.styles import getSampleStyleSheet
 from reportlab.platypus import SimpleDocTemplate, Paragraph
+from vosk import Model, KaldiRecognizer
+import wave
+import json
+import subprocess
 
-from gpt_call import image_call, text_call
-from app import get_notes, update_notes, read_notes
+from functions.gpt_call import image_call, text_call
+from functions.app import get_notes, update_notes, read_notes
+from functions.conversions import image, pdf
 
-from app import app, update_notes
+from functions.app import app, update_notes
 from threading import Thread
 import threading
 
@@ -29,22 +34,19 @@ flask_thread.start()
 # Start the file watcher in a background thread
 threading.Thread(target=read_notes, daemon=True).start()
 
-# Update the notes dynamically while Flask is running
-#update_notes("Getting note...")
 print("Flask is running, but main.py can still execute other code.")
 
 
 def main():
 
+    # Uploaded Files
     folder = "notes"
     files = os.listdir(folder) # List of files in notes folder 
     if ".DS_Store" in files:
         files.remove(".DS_Store")
 
-    # Text file of all GPT outputs
-    output_text = "notes.txt" 
-    # Ensure the file exists by creating it if it doesnt
-    #open("notes.txt", "w").close()    
+    output_text = "notes.txt"   # Text file of all GPT note outputs
+    open("notes.txt", "w").close()  # Ensure the file exists by creating it if it doesnt
 
     # Folder of converted pngs
     pngs_folder = "converted_pngs" 
@@ -60,32 +62,23 @@ def main():
         file_type = file_type.upper()
         print(f"{file_path} | {file_type}")
 
+
+        valid_video_types = {"MP4", "AVI", "MKV", "MOV", "WMV", "FLV", "WEBM", "MPEG", "MPG", "OGV", "3GP", "MTS"}
+        valid_image_types = {"PNG", "JPEG", "JPG", "BMP", "GIF", "TIFF", "WEBP"}
+
         try:
             # For Images
-            valid_image_types = {"PNG", "JPEG", "JPG", "BMP", "GIF", "TIFF", "WEBP"}
             if file_type in valid_image_types:
-
-                img = Image.open(file_path) # Open the current image 
-                img = img.convert("RGBA") # Convert to RGBA
-
-                # Create the output filename with a .png extention
-                base_name = os.path.splitext(file)[0] + ".png"
-                output_file = os.path.join(pngs_folder, base_name)
-                img.save(output_file, "PNG")
-                #print(f"Output file:{output_file}"
-                
-                '''
-                if confidence low
-                    img = img.resize((512, 512), Image.LANCZOS)
-                '''
+                image(file)
                 
             # For PDFs
             elif file_type == 'PDF':
-                images = convert_from_path(file_path) # Convert PDF pages to images
-                images = images.resize((256, 256), Image.LANCZOS)
-                # Save each page as a PNG in the output folder
-                for i, image in enumerate(images):
-                    image.save(os.path.join(pngs_folder, f"page_{i+1}.png"), "PNG")
+                pdf(file)
+
+            # For videos
+            elif file_type in valid_video_types:
+                #video(file)
+                print()
 
         except Exception as e:
             print(f"Invalid file format {e}")
@@ -102,28 +95,22 @@ def main():
         # Convert to OCR
         file_path = os.path.join(pngs_folder, file)
         image = Image.open(file_path) # Open image
-        #text = pytesseract.image_to_string(image) # Convert image to text
         ocr_data = pytesseract.image_to_data(image, output_type=pytesseract.Output.DATAFRAME) # Run OCR with detailed output
         #print(ocr_data)
 
-        # Filter out NaN text and low-confidence words
-        important_text = ocr_data[(ocr_data["text"].notna()) & (ocr_data["conf"] > 50)]
-        # Extract the text as a list or single string
-        extracted_text = " ".join(important_text["text"])
-
+        important_text = ocr_data[(ocr_data["text"].notna()) & (ocr_data["conf"] > 50)] # Filter out NaN text and low-confidence words
+        extracted_text = " ".join(important_text["text"]) # Extract the text as a list or single string
         #print(f"Extracted text: {extracted_text}")
 
         # Create a text file
-
         filename = f"output{counter}.txt"
         txt_path = os.path.join(txt_folder, filename)  # Create full file path inside the folder
 
-        # Try to open the file in write mode with 
+        # Try to open the file in write mode 
         with open(txt_path, "w", encoding="utf-8") as file:
             file.write(extracted_text + "\n")
             counter +=1
 
-        # If low confidence, load to new file and use GPT Vision
         word_count = extracted_text.split() # Split text on spaces into a list
         word_count = len(word_count) # Get length of the list
         #print(f"Word count: {word_count}")
@@ -139,28 +126,24 @@ def main():
             img.save(file_path)
 
             #print("Calling GPT Vision")
-
-            response_content = "hi"
-            #image_call(file_path)
+            response_content = ""
+            image_call(file_path)
             with open(txt_path, "a", encoding="utf-8") as file:
                 file.write(response_content + "\n")
-        # Else, send OCR text to GPT text
-        else:
-            None
+
 
     # Loop thru every txt file containing notes
     for filename in os.listdir(txt_folder):  
         file_path = os.path.join(txt_folder, filename)  # Get the full path
         time.sleep(5)
-        #print("Calling GPT")
-        #text_call(file_path) 
+        print("Calling GPT")
+        text_call(file_path) 
 
     # Create a PDF
     def create_pdf(filename, text):
         doc = SimpleDocTemplate(filename, pagesize=letter)
         styles = getSampleStyleSheet()
         story = []
-
         # Split text into paragraphs (preserving newlines)
         for paragraph in text.split("\n\n"):  # Splits at paragraphs (double newlines for paragraphs)
             story.append(Paragraph(paragraph.replace("\n", "<br/>"), styles["Normal"]))
